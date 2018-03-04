93d3cea914a588df71ac8b0d79475276b81889b4
GionataB <gionatabonazzi@gmail.com>
2018-03-04 17:59:29 -0500

    Changed the list files and added comments.

--- Product_MS_project2/LLbonazzi.c
+++ Product_MS_project2/LLbonazzi.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<stdlib.h>
 #include <string.h>
 #include "file_utilities.h"
 #include "project2.h"
@@ -8,7 +10,7 @@ struct node* init(){
   return head;
 }
 
-void insert(struct node *head, char* name, char* unit, int price, int quantity){
+void insert(struct node* head, char* name, char* unit, int price, int quantity){
   if(head->next == NULL){
     head->next = (struct node*) malloc(sizeof(struct node*));
     head->next->name = name;
@@ -21,7 +23,7 @@ void insert(struct node *head, char* name, char* unit, int price, int quantity){
     insert(head->next, name, unit, price, quantity); //Recursion
 }
 
-void delete(struct node *head, char* name){
+void delete(struct node* head, char* name){
   if(strcmp(head->next->name, name)){
     struct node *temp = head->next;
     head->next = head->next->next;
@@ -31,7 +33,7 @@ void delete(struct node *head, char* name){
     delete(head->next, name);
 }
 
-struct node* search(struct node *head, char* name){
+struct node* search(struct node* head, char* name){
   if(head->next == NULL){
     printf("Not Found");
     return NULL;
@@ -43,21 +45,21 @@ struct node* search(struct node *head, char* name){
     search(head->next, name);
 }
 
-void displayProduct(struct node *product){
+void displayProduct(struct node* product){
   printf("Product's name:\t%s\n", head->next->name);
   printf("Product's unit:\t%s\n", head->next->unit);
   printf("Product's price:\t%d\n", head->next->price);
   printf("Product's quantity:\t%d\n\n", head->next->quantity);
 }
 
-void displayAll(struct node *head){
+void displayAll(struct node* head){
   if(head->next != NULL){ //starts from head->next so the head's data won't be printed
     displayProduct(head->next);
     display(head->next); //Recursion
   }
 }
 
-void purchase(struct node *head, char* name){
+void purchase(struct node* head, char* name){
   struct node* temp = search(head, name);
   temp->quantity++;
 }
@@ -69,31 +71,34 @@ void sell(struct node *head, char* name){
     delete(head, name);
 }
 
-void save(struct node *head, char* filename, int size){
+void save(struct node* head, char* filename){
+  int size = 19 + (listSize(head) * 160) + 100;//FIXME: create the method listSize, and add the constants to that.
+  //FIXME: the formula for the size: 19 + (listSize() * 160);
+  //FIXME: the 100 is to have a bit of room.
   char* str = (char*) malloc(size*sizeof(char));
-  strcpy(str, "Saved Linked List\n");
+  strcpy(str, "Saved Linked List\n");//18 chars, constant
   for(int i = 1; head->next->next != NULL; head = head->next, i++){
-    strcat(str, "Element number: ");
-    strcat(str, i);
-    strcat(str, "\n");
-    strcat(str, "name: ");
-    strcat(str, head->next->name);
-    strcat(str, "\t");
-    strcat(str, "unit: ");
-    strcat(str, head->next->unit);
-    strcat(str, "\t");
-    strcat(str, "price: ");
-    strcat(str, head->next->price);
-    strcat(str, "\t");
-    strcat(str, "quantity: ");
-    strcat(str, head->next->quantity);
-    strcat(str, "\n");
-  }
-  write_file(filename, str, size);
+    strcat(str, "Element number: ");//16 chars per node, constant
+    strcat(str, i);//variable, max of 4 chars. (max 9999 nodes in the list)
+    strcat(str, "\n");//1 char per node, constant.
+    strcat(str, "name: ");//6 chars per node, constant.
+    strcat(str, head->next->name);//variable, max of 50 chars. (a name 50 letters long max)
+    strcat(str, "\t");//1 char per node, constant
+    strcat(str, "unit: ");//6 chars per node, constant
+    strcat(str, head->next->unit);//variable, max of 50 chars. (a type of product 50 letters long max)
+    strcat(str, "\t");//1 char per node, constant
+    strcat(str, "price: ");//7 chars per node, constant
+    strcat(str, head->next->price);//variable, max of 6 chars. (999999 is the max price)
+    strcat(str, "\t");//1 char per node, constant
+    strcat(str, "quantity: ");//10 chars per node, constant
+    strcat(str, head->next->quantity);//variable, max of 4 chars. (max 9999 items per node)
+    strcat(str, "\n");//1 char per node, constant
+  } //total: 160 chars per node.
+  strcat(str, "\0");//1 char, constant
+  int strSize = strlen(str); //To have a more precise size to pass to write_file.
+  write_file(filename, str, strSize);
   free(str);
 }
-
-
 
 void deleteAll(struct node *head){
   if(head != NULL){
--- Product_MS_project2/LinkedList.h
+++ Product_MS_project2/LinkedList.h
@@ -1,9 +1,12 @@
-#include<stdio.h>
-#include<stdlib.h>
+#ifndef LL_PROJECT2_H
+#define LL_PROJECT2_H
 
-#ifndef LAB3_H
-#define LAB3_H
-
+/*******************************************************************************
+* A linked list structure. For each node, the linked list saves a name, a unit,
+* a price and a quantity, as well as the pointer to the next node in the list.
+* the name and the unit are strings.
+* the price and the quantity are integers.
+*******************************************************************************/
 struct node
 {
 	char* name;
@@ -13,12 +16,80 @@ struct node
 	struct node *next;
 };
 
-struct node *init (); //initialize: create an empty head node (whose "data" is intentionally missing); This head node will not be used to store any data;
+/*******************************************************************************
+* Initialize the head of a linked list. The head of the list contains only
+* a pointer to the next node, it does not contain any data.
+* @return a pointer to the head of the linked list.
+*******************************************************************************/
+struct node *init ();
 
-void insert(struct node *head, int data); //Create a new node to store data and insert it to the end of current linked list; the head node will still be empty and data in the array are not stored in head node
+/*******************************************************************************
+* Insert a new node at the end of the linked list pointed by head.
+* @param head the head to the linked list.
+* @param name the name of the product saved in the node.
+* @param unit the unit of the product saved in the node.
+* @param price the price per item of the product saved in the node.
+* @param quantity the number of items of the same product available.
+*******************************************************************************/
+void insert(struct node* head, char* name, char* unit, int price, int quantity); //Create a new node to store data and insert it to the end of current linked list; the head node will still be empty and data in the array are not stored in head node
 
-void display (struct node *head); //print data for all nodes in the linked list except the head node (which is empty)
+/*******************************************************************************
+* Delete the desired node from the list.
+* @param head the head to the linked list.
+* @param name the name of the product to delete.
+*******************************************************************************/
+void delete(struct node* head, char* name);
 
-void deleteAll (struct node *head); //delete the entire linked list including the head node
+/*******************************************************************************
+* Search the list for the product whose name is the same as the name provided.
+* A different message will be showed on the console if the product is found, or
+* it is not.
+* @param head the head to the linked list.
+* @param name the name of the product to search.
+* @return A pointer to the node if the product is in the list, or null if the
+*					product is not in the list.
+*******************************************************************************/
+struct node* search(struct node* head, char* name);
+
+/*******************************************************************************
+* Print all the data of the given node.
+* @param product the node whose data are to be printed.
+*******************************************************************************/
+void displayProduct(struct node* product);
+
+/*******************************************************************************
+* Print all the data of all the nodes in the list.
+* @param head the pointer to the head of the list.
+*******************************************************************************/
+void displayAll(struct node* head);
+
+/*******************************************************************************
+* Add one to the quantity of a node.
+* @param head the head to the linked list.
+* @param name the name of the product to increment.
+*******************************************************************************/
+void purchase(struct node* head, char* name);
+
+/*******************************************************************************
+* Decrease the quantity of an item by one.
+* If the quantity of the item after selling is zero, the node gets deleted.
+* @param head the head to the linked list.
+* @param name the name of the product to decrease.
+*******************************************************************************/
+void sell(struct node *head, char* name);
+
+/*******************************************************************************
+* Save the data of all the nodes in the list on a file.
+* @param head the head to the linked list.
+* @param filename the name of the file.
+* @param size the total size of the data to be saved.
+*******************************************************************************/
+void save(struct node* head, char* filename, int size);
+
+/*******************************************************************************
+* Delete the entire linked list including the head.
+* @param head the head to the linked list.
+*******************************************************************************/
+void deleteAll (struct node *head);
 
 #endif
