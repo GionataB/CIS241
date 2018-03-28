762ef9d65f969a44a5d0a26fb0f08a9c501b0385
GionataB <gionatabonazzi@gmail.com>
2018-03-28 18:49:18 -0400

    Added project 3, does not have any comment in it, not been tested yet.

---
+++ Backup_Shell_project3/backup.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+if test $# -lt 2
+  then
+    echo “must provide at least one argument”
+    exit 1
+fi
+target="~/backup"
+diskUsage="-c"
+listContent="-l"
+askHelp="--help"
+if test -e "$target"
+  then
+  else
+    mkdir "$target"
+fi
+for i in $#
+  if test -e "$i"
+    then
+      mv "$i" "$target"
+  elif test "$i" = "$diskUsage"
+    then
+      echo "There are $(ls | wc -w) files and directories in the backup folder."
+      echo "The backup folder occupies $(du -b $target) bytes." #might want to put quotes around $target
+  elif test "$i" = "$listContent"
+    then
+      ls "$target"
+  elif test "$i" = "$askHelp"
+    then
+      echo "Syntax: ./backup targetFileList [options]\ntargetFileList is one or more file to copy\nAvailable options are:\n-c to get the number of files and directories inside the backup directory, and the total size in bytes.\n-l to list all files and directories in the backup directory.\n--help to display this message."
+  else
+    echo "$i does not exist!"
+  fi
