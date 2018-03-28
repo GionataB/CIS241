f2a5d0e815ad7309cff5bff4a1148485e9c52aa0
GionataB <gionatabonazzi@gmail.com>
2018-03-28 19:37:56 -0400

    Fixed the for in syntax

--- Backup_Shell_project3/backup.sh
+++ Backup_Shell_project3/backup.sh
@@ -1,32 +1,35 @@
 #! /bin/bash
-if test $# -lt 2
+if test $# -lt 1
   then
-    echo “must provide at least one argument”
+    echo "must provide at least one argument"
     exit 1
 fi
-target="~/backup"
+target=~/backup
 diskUsage="-c"
 listContent="-l"
 askHelp="--help"
 if test -e "$target"
   then
+    echo "The directory already exists, its content may be overwritten."
   else
     mkdir "$target"
 fi
 for i in $#
-  if test -e "$i"
+do
+  if test -e $i
     then
-      cp -r "$i" "$target"
-  elif test "$i" = "$diskUsage"
+      cp -r $i "$target"
+  elif test $i = $diskUsage
     then
       echo "There are $(ls | wc -w) files and directories in the backup folder."
       echo "The backup folder occupies $(du -b $target) bytes." #might want to put quotes around $target
-  elif test "$i" = "$listContent"
+  elif test $i = $listContent #the variables should be string, but maybe wrap the two variables in quotiation marks
     then
       ls "$target"
-  elif test "$i" = "$askHelp"
+  elif test $i = $askHelp
     then
       echo "Syntax: ./backup targetFileList [options]\ntargetFileList is one or more file to copy\nAvailable options are:\n-c to get the number of files and directories inside the backup directory, and the total size in bytes.\n-l to list all files and directories in the backup directory.\n--help to display this message."
   else
     echo "$i does not exist!"
   fi
+done
