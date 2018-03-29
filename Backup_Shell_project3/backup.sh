36812d902ed464148148fc6f87b3b88fa0306849
GionataB <gionatabonazzi@gmail.com>
2018-03-28 20:17:28 -0400

    Fixed -c command

--- Backup_Shell_project3/backup.sh
+++ Backup_Shell_project3/backup.sh
@@ -14,19 +14,19 @@ if test -e "$target"
   else
     mkdir "$target"
 fi
-for i in $#
+for i
 do
-  if test -e $i
+  if test -e "$i"
     then
-      cp -r $i "$target"
-  elif test $i = $diskUsage
+      cp -r "$i" "$target"
+  elif test "$i" = "$diskUsage"
     then
       echo "There are $(ls | wc -w) files and directories in the backup folder."
-      echo "The backup folder occupies $(du -b $target) bytes." #might want to put quotes around $target
-  elif test $i = $listContent #the variables should be string, but maybe wrap the two variables in quotiation marks
+      echo "The backup folder occupies $(du -c -b $target) bytes." #might want to put quotes around $target
+  elif test "$i" = "$listContent" #the variables should be string, but maybe wrap the two variables in quotiation marks
     then
       ls "$target"
-  elif test $i = $askHelp
+  elif test "$i" = "$askHelp"
     then
       echo "Syntax: ./backup targetFileList [options]\ntargetFileList is one or more file to copy\nAvailable options are:\n-c to get the number of files and directories inside the backup directory, and the total size in bytes.\n-l to list all files and directories in the backup directory.\n--help to display this message."
   else
