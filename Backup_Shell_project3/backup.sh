208fc1f36d9082377332488f797c09090f6865e9
GionataB <gionatabonazzi@gmail.com>
2018-03-29 11:58:52 -0400

    fixed the --help command

--- Backup_Shell_project3/backup.sh
+++ Backup_Shell_project3/backup.sh
@@ -22,13 +22,13 @@ do
   elif test "$i" = "$diskUsage"
     then
       echo "There are $(ls | wc -w) files and directories in the backup folder."
-      echo "The backup folder occupies $(du -s -b $target | cut -f 1) bytes." #might want to put quotes around $target
+      echo "The backup folder occupies $(du -s -b $target | cut -f 1) bytes."
   elif test "$i" = "$listContent" #the variables should be string, but maybe wrap the two variables in quotiation marks
     then
       ls "$target"
   elif test "$i" = "$askHelp"
     then
-      echo "Syntax: ./backup targetFileList [options]\ntargetFileList is one or more file to copy\nAvailable options are:\n-c to get the number of files and directories inside the backup directory, and the total size in bytes.\n-l to list all files and directories in the backup directory.\n--help to display this message."
+      echo -e "Syntax: ./backup targetFileList [options]\rtargetFileList is one or more file to copy\rAvailable options are:\r-c to get the number of files and directories inside the backup directory, and the total size in bytes.\r-l to list all files and directories in the backup directory.\r--help to display this message."
   else
     echo "$i does not exist!"
   fi
