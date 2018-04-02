cd4cd83af00b17d964b91e37b85fea616a9240d4
GionataB <gionatabonazzi@gmail.com>
2018-04-02 19:40:53 -0400

    Fixed the mkdir command

--- Test_Shell_Lab5/test.sh
+++ Test_Shell_Lab5/test.sh
@@ -8,17 +8,20 @@ target=~/backup
 diskUsage="-c"
 listContent="-l"
 askHelp="--help"
-mkdir "$target" #The current version on the eos machines makes a directory if it does not already exists.
-if test "$0" = "$diskUsage"
+if test -ne "$target"
+  then
+    mkdir "$target"
+fi
+if test "$1" = "$diskUsage"
   then
     echo "There are $(ls | wc -w) files and directories in the current path, $(pwd)."
     echo "The files and directories in the current path occupy $(du -s -b ".." | cut -f 1) bytes."
-elif test "$0" = "$listContent" #the variables should be string, but maybe wrap the two variables in quotiation marks
+elif test "$1" = "$listContent" #the variables should be string, but maybe wrap the two variables in quotiation marks
   then
     ls
-elif test "$0" = "$askHelp"
+elif test "$1" = "$askHelp"
   then
     echo -e "Syntax: ./test.sh [options]\nUsage:\t-c to get the number of files and directories inside the current path, and the total size in bytes.\n\t-l to list all files and directories in the current path.\n\t--help to display this message."
   else
-    echo "$0 does not exist!"
+    echo "$1 does not exist!"
 fi
