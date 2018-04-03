01ecab9ba8a81cecab3eec4e65774d9e7168b273
GionataB <gionatabonazzi@gmail.com>
2018-04-02 21:43:58 -0400

    Script working.

--- Test_Shell_Lab5/test.sh
+++ Test_Shell_Lab5/test.sh
@@ -8,20 +8,25 @@ target=~/backup
 diskUsage="-c"
 listContent="-l"
 askHelp="--help"
+#First Task
+if test "$1" = "$diskUsage"
+  then
+    echo "The number of files"
+    echo "How many bytes"
+elif test "$1" = "$listContent" #the variables should be string, but maybe wrap the two variables in quotiation marks
+  then
+    echo "List all the files."
+elif test "$1" = "$askHelp"
+  then
+    echo -e "How to run this script."
+  else
+    echo "$1 does not exist!"
+fi
+#Second Task
 if test -ne "$target"
   then
     mkdir "$target"
 fi
-if test "$1" = "$diskUsage"
-  then
-    echo "There are $(ls | wc -w) files and directories in the current path, $(pwd)."
-    echo "The files and directories in the current path occupy $(du -s -b ".." | cut -f 1) bytes."
-elif test "$1" = "$listContent" #the variables should be string, but maybe wrap the two variables in quotiation marks
-  then
-    ls
-elif test "$1" = "$askHelp"
-  then
-    echo -e "Syntax: ./test.sh [options]\nUsage:\t-c to get the number of files and directories inside the current path, and the total size in bytes.\n\t-l to list all files and directories in the current path.\n\t--help to display this message."
-  else
-    echo "$1 does not exist!"
-fi
+#Third Task
+echo "There are $(ls | wc -w) files and directories in the current path, $(pwd)."
+echo "The files and directories in the current path occupy $(du -s -b ".." | cut -f 1) bytes."
